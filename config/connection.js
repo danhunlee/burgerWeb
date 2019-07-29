0eabceff601b49b4149ace1dadd2f8df8818aa8c
Hun Lee <dan.hun.lee@gmail.com>
2019-07-29 14:23:51 -0700

    hl

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if(process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
     host: "localhost",
-    port: 3300,
+    port: 3306,
     user: "root",
     password: "dlgjs123!",
     database: "burgers_db"
